38970ab4dae8c97dae94aeb421c829e65896a237
Salekeen <nawazsalekeennayeem@gmail.com>
2022-11-19 01:22:47 +0600

    added docs

--- src/solution.py
+++ src/solution.py
@@ -9,6 +9,19 @@ from utils import clean
 
 
 def get_new_df_augmented(df):
+    """
+    It takes a dataframe with a column called 'Question' and returns a new dataframe with a column
+    called 'Answer' and 'Index'
+    
+    The 'Answer' column is a list of sentences from the 'Question' column
+    
+    Args:
+      df: the dataframe that you want to augment
+    
+    Returns:
+      A dataframe with the questions split into sentences and the index of the original question.
+    """
+    
     new_df = pd.DataFrame(columns=['Answer', 'Index'])
     texts = []
     indexes = []
@@ -24,12 +37,36 @@ def get_new_df_augmented(df):
 
 
 def replace_synonymns(df_aug,test_df):
+    """
+    It takes in the augmented dataframe and the test dataframe, and replaces the synonyms in the
+    augmented dataframe's answers and the test dataframe's questions
+    
+    Args:
+      df_aug: the augmented dataframe
+      test_df: The test dataframe
+    
+    Returns:
+      the augmented dataframe and the test dataframe.
+    """
+    
     df_aug['Answer'] = df_aug['Answer'].map(clean.get_synonyms)
     test_df['Question'] = test_df['Question'].map(clean.get_synonyms)
     return df_aug,test_df
 
 
 def get_embeddiings(train_df, test_df):
+    """
+    It takes the answers and questions from the training and test dataframes, and returns the embeddings
+    for each of them
+    
+    Args:
+      train_df: the training dataframe
+      test_df: the dataframe containing the questions
+    
+    Returns:
+      The embeddings of the answers and questions
+    """
+    
     model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
     answers = train_df['Answer']
     answer_embedding = model.encode(answers)
@@ -41,9 +78,22 @@ def get_embeddiings(train_df, test_df):
 
 
 def check_similarity(answer_embed,target_embed,df_aug,test_df,df):
+    """
+    It takes the embeddings of the target question and the answer embeddings and finds the most similar
+    answer embedding to the target embedding. 
+    
+    The index of the most similar answer embedding is then used to find the answer in the original
+    dataframe
+    
+    Args:
+      answer_embed: The embeddings of the answers in the training set
+      target_embed: The embeddings of the questions in the test set
+      df_aug: The augmented dataframe
+      test_df: The dataframe containing the questions to be answered
+      df: The original dataframe
+    """
+    
     for i in range(len(target_embed)):
-
-
         index = cosine_similarity(
             [target_embed[i]],
             answer_embed[:]
--- src/utils/clean.py
+++ src/utils/clean.py
@@ -1,5 +1,15 @@
 import re
 def get_synonyms(text):
+    """
+    It replaces the word "sibling" with "brother and sister" and the word "parent" with "father and
+    mother"
+    
+    Args:
+      text: The text to be processed.
+    
+    Returns:
+      The text with the synonyms
+    """
     text = re.sub(r'[Ss]ibling[s]',r'brother and sister',text)
     text = re.sub(r'[Pp]arent[s]',r'father and mother',text)
     return text
